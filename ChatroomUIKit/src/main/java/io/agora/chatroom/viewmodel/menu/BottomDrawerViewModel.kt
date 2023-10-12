a78fc0305cf38898ba6f08fb0ab9471518a8bb69
jinan <neil.liu@easemob.com>
2023-10-12 18:24:58 +0800

    Optimize ComposeBottomDrawer.kt.

---
+++ ChatroomUIKit/src/main/java/io/agora/chatroom/viewmodel/menu/BottomDrawerViewModel.kt
@@ -0,0 +1,95 @@
+package io.agora.chatroom.viewmodel.menu
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.toMutableStateList
+import androidx.lifecycle.ViewModel
+import io.agora.chatroom.uikit.R
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+/**
+ * The view model of the bottom drawer.
+ */
+open class BottomDrawerViewModel<T> (
+    private val isDarkTheme: Boolean? = false,
+    private val isShowTitle:Boolean = true,
+    private val isShowCancel:Boolean = true,
+    private val title:String = "",
+    private val cancel:Int = R.string.compose_bottom_drawer_cancel,
+    private val contentList: List<T> = emptyList(),
+): ViewModel() {
+
+    /**
+     * Control whether the bottom drawer can be drawn.
+     */
+    private val _enable = mutableStateOf(false)
+    /**
+     * Check whether the bottom drawer is visible.
+     */
+    private val _isVisible = mutableStateOf(false)
+
+    private val _contentList: MutableList<T> = contentList.toMutableStateList()
+    val list: List<T> = _contentList
+
+    private val _show : MutableState<Boolean> = mutableStateOf(false)
+    var isBottomDrawerVisible = _show
+
+    private val _drawerShouldBeOpened = MutableStateFlow(false)
+    val drawerShouldBeOpened = _drawerShouldBeOpened.asStateFlow()
+
+    val getTheme: Boolean?
+        get() = isDarkTheme
+
+    val getTitle: String
+        get() = title
+
+    val getCancelText: Int
+        get() = cancel
+
+    val getIsShowTitle: Boolean
+        get() = isShowTitle
+
+    val getIsShowCancel: Boolean
+        get() = isShowCancel
+
+    fun openDrawer(){
+        _show.value = true
+        _enable.value = true
+    }
+
+    fun closeDrawer(){
+        _show.value = false
+    }
+
+    fun openBottomDrawer() {
+        _drawerShouldBeOpened.value = true
+    }
+
+    fun resetOpenDrawerAction() {
+        _drawerShouldBeOpened.value = false
+    }
+
+    /**
+     * Set the bottom drawer visible or not.
+     */
+    fun setVisible(isVisible: Boolean) {
+        _isVisible.value = isVisible
+    }
+
+    /**
+     * Check whether the bottom drawer is visible.
+     */
+    fun isVisible(): Boolean {
+        return _isVisible.value
+    }
+
+    fun setEnable(enable: Boolean) {
+        _enable.value = enable
+    }
+
+    fun isEnable(): Boolean {
+        return _enable.value
+    }
+
+}
